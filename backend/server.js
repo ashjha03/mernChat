f89d4b44c37a63689b8761296f7a336b4cf3931b
Ashish Jha <88213707+ashjha03@users.noreply.github.com>
2024-04-24 16:39:28 +0530

    Authentication Routes created

--- backend/server.js
+++ backend/server.js
@@ -2,24 +2,19 @@ const express = require("express");
 const dotenv = require("dotenv");
 const chats = require("./data/data");
 const connectDB = require("./config/db");
+const userRoutes = require("./routes/userRoutes");
 
 const app = express();
 dotenv.config();
 connectDB();
 
+app.use(express.json());
+
 app.get("/", (req, res) => {
   res.send("Hi");
 });
 
-app.get("/api/chat", (req, res) => {
-  res.send(chats);
-});
-
-app.get("/api/chat/:id", (req, res) => {
-  const id = req.params.id;
-  const chat = chats.find((ele) => ele._id === id);
-  res.send(chat);
-});
+app.use("/api/user", userRoutes);
 
 const PORT = process.env.POST || 5000;
 app.listen(5000, console.log(`Server started on PORT ${PORT}`));
